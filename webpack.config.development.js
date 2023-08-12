b74c9c9578916d4740224c016c1f972a26d46bc6
Athenkosi-rgb <82506682+Athenkosi-rgb@users.noreply.github.com>
2023-08-12 05:50:24 +0200

    Initial commit

---
+++ webpack.config.development.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const path = require('path');
+const config = require('./webpack.config');
+
+module.exports = merge(config, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, ''), // Change this to your desired output directory
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+  },
+});
